150090bce23bcce30d9388bf8c289ce41ac7f4b2
russ-davey <russell.j.davey@gmail.com>
2022-03-18 15:13:51 +0000

    Refactor - ensuring intent of code is clear

--- files/store.go
+++ files/store.go
@@ -19,6 +19,11 @@ import (
 	"github.com/ONSdigital/dp-upload-service/upload"
 )
 
+const (
+	StateUploaded = "UPLOADED"
+	vaultKey      = "key"
+)
+
 var (
 	ErrFilesAPIDuplicateFile    = errors.New("files API already contains a file with this path")
 	ErrFileAPICreateInvalidData = errors.New("invalid data sent to Files API")
@@ -30,10 +35,6 @@ var (
 	ErrChunkTooSmall            = errors.New("chunk size below minimum 5MB")
 	ErrVaultWrite               = errors.New("failed to write to vault")
 	ErrVaultRead                = errors.New("failed to read from vault")
-)
-
-const (
-	vaultKey = "key"
 )
 
 type Store struct {
@@ -48,120 +49,110 @@ func NewStore(hostname string, s3 upload.S3Clienter, keyGenerator encryption.Gen
 	return Store{hostname, s3, keyGenerator, vault, vaultPath}
 }
 
-type StoreMetadata struct {
-	Path          string  `json:"path"`
-	IsPublishable bool    `json:"is_publishable"`
-	CollectionId  *string `json:"collection_id,omitempty"`
-	Title         string  `json:"title"`
-	SizeInBytes   int     `json:"size_in_bytes"`
-	Type          string  `json:"type"`
-	Licence       string  `json:"licence"`
-	LicenceUrl    string  `json:"licence_url"`
-}
-
-type Resumable struct {
-	FileName     string `schema:"resumableFilename"`
-	Type         string `schema:"resumableType"`
-	CurrentChunk int64  `schema:"resumableChunkNumber"`
-	TotalChunks  int    `schema:"resumableTotalChunks"`
-}
-
-type uploadComplete struct {
-	State string `json:"state"`
-	ETag  string `json:"etag"`
-}
-
-type jsonError struct {
-	Code        string `json:"code"`
-	Description string `json:"description"`
-}
-
-type jsonErrors struct {
-	Error []jsonError `json:"errors"`
-}
-
-func firstChunk(currentChunk int64) bool { return currentChunk == 1 }
-
 func (s Store) UploadFile(ctx context.Context, metadata StoreMetadata, resumable Resumable, content []byte) (bool, error) {
+	var encryptionKey []byte
+	var err error
 
-	var encryptionkey []byte
-	vaultPath := fmt.Sprintf("%s/%s", s.vaultPath, metadata.Path)
 	if firstChunk(resumable.CurrentChunk) {
-		encryptionkey = s.keyGenerator()
-
-		if err := s.registerFileUpload(metadata); err != nil {
+		if err = s.registerFileUpload(metadata); err != nil {
 			log.Error(ctx, "failed to register file metadata with dp-files-api", err, log.Data{"metadata": metadata})
 			return false, err
 		}
 
-		if err := s.vault.WriteKey(vaultPath, vaultKey, hex.EncodeToString(encryptionkey)); err != nil {
-			log.Error(ctx, "failed to write encryption encryptionkey to vault", err, log.Data{"vault-path": vaultPath, "vault-encryptionkey": vaultKey})
-			return false, ErrVaultWrite
+		encryptionKey, err = s.generateEncryptionKey(ctx, metadata.Path)
+		if err != nil {
+			return false, err
 		}
 	} else {
-		strKey, err := s.vault.ReadKey(vaultPath, vaultKey)
+		encryptionKey, err = s.getEncryptionKey(ctx, metadata.Path)
 		if err != nil {
-			log.Error(ctx, "failed to read encryption encryptionkey from vault", err, log.Data{"vault-path": vaultPath, "vault-encryptionkey": vaultKey})
-			return false, ErrVaultRead
+			return false, err
 		}
-
-		encryptionkey, _ = hex.DecodeString(strKey)
 	}
 
-	upr := s3client.UploadPartRequest{
-		UploadKey:   metadata.Path,
-		Type:        resumable.Type,
-		ChunkNumber: resumable.CurrentChunk,
-		TotalChunks: resumable.TotalChunks,
-		FileName:    resumable.FileName,
-	}
-
-	response, err := s.s3.UploadPartWithPsk(ctx, &upr, content, encryptionkey)
+	part := s.generateUploadPart(metadata, resumable)
+	response, err := s.s3.UploadPartWithPsk(ctx, part, content, encryptionKey)
 	if err != nil {
-		log.Error(ctx, "failed to write chunk to s3", err, log.Data{"s3-upload-part": upr})
+		log.Error(ctx, "failed to write chunk to s3", err, log.Data{"s3-upload-part": part})
 		if _, ok := err.(*s3client.ErrChunkTooSmall); ok {
 			return false, ErrChunkTooSmall
 		}
 		return false, ErrS3Upload
 	}
 
-	uc := uploadComplete{
-		State: "UPLOADED",
-		ETag:  strings.Trim(response.Etag, "\""),
-	}
-
 	if response.AllPartsUploaded {
-		if err := s.markUploadComplete(metadata.Path, uc); err != nil {
-			log.Error(ctx, "failed to mark upload complete with dp-files-api", err, log.Data{"upload-complete": uc})
-			return true, err
-		}
+		return true, s.markUploadComplete(ctx, metadata.Path, response.Etag)
 	}
 
-	return response.AllPartsUploaded, nil
+	return false, nil
 }
 
-func (s Store) markUploadComplete(path string, uc uploadComplete) error {
-	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodPatch, fmt.Sprintf("%s/files/%s", s.hostname, path), jsonEncode(uc))
-	if err != nil {
-		return err
+func (s Store) generateEncryptionKey(ctx context.Context, filepath string) ([]byte, error) {
+	encryptionKey := s.keyGenerator()
+	if err := s.vault.WriteKey(s.getVaultPath(filepath), vaultKey, hex.EncodeToString(encryptionKey)); err != nil {
+		log.Error(ctx, "failed to write encryption encryptionKey to vault", err, log.Data{"vault-path": s.getVaultPath(filepath), "vault-encryptionKey": vaultKey})
+		return nil, ErrVaultWrite
 	}
 
+	return encryptionKey, nil
+}
+
+func (s Store) getEncryptionKey(ctx context.Context, filepath string) ([]byte, error) {
+	strKey, err := s.vault.ReadKey(s.getVaultPath(filepath), vaultKey)
+	if err != nil {
+		log.Error(ctx, "failed to read encryption encryptionkey from vault", err, log.Data{"vault-path": s.getVaultPath(filepath), "vault-encryptionkey": vaultKey})
+		return nil, ErrVaultRead
+	}
+
+	encryptionKey, _ := hex.DecodeString(strKey)
+	return encryptionKey, nil
+}
+
+func (s Store) getVaultPath(filepath string) string {
+	return fmt.Sprintf("%s/%s", s.vaultPath, filepath)
+}
+
+func firstChunk(currentChunk int64) bool { return currentChunk == 1 }
+
+func (s Store) generateUploadPart(metadata StoreMetadata, resumable Resumable) *s3client.UploadPartRequest {
+	return &s3client.UploadPartRequest{
+		UploadKey:   metadata.Path,
+		Type:        resumable.Type,
+		ChunkNumber: resumable.CurrentChunk,
+		TotalChunks: resumable.TotalChunks,
+		FileName:    resumable.FileName,
+	}
+}
+
+func (s Store) markUploadComplete(ctx context.Context, path, etag string) error {
+	uc := uploadComplete{StateUploaded, strings.Trim(etag, "\"")}
+
+	client := &http.Client{}
+	req, _ := http.NewRequest(http.MethodPatch, fmt.Sprintf("%s/files/%s", s.hostname, path), jsonEncode(uc))
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
+
+	logData := log.Data{"upload-complete": uc, "request": req, "response": resp}
 	if err != nil {
+		log.Error(ctx, fmt.Sprintf("making patch request to mark file %s", StateUploaded), err, logData)
 		return ErrConnectingToFilesApi
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound {
-		return ErrFileNotFound
+		err := ErrFileNotFound
+		log.Error(ctx, "could not file file to mark uploaded", err, logData)
+		return err
 	} else if resp.StatusCode == http.StatusConflict {
-		return ErrFileStateConflict
+		err := ErrFileStateConflict
+		log.Error(ctx, "file in wrong state to be marked uploaded", err, logData)
+		return err
 	} else if resp.StatusCode != http.StatusOK {
-		return ErrUnknownError
+		err := ErrUnknownError
+		log.Error(ctx, "unexpected error morning file uploaded", err, logData)
+		return err
 	}
 
 	return nil
@@ -202,4 +193,36 @@ func jsonEncode(data interface{}) *bytes.Buffer {
 	b := &bytes.Buffer{}
 	json.NewEncoder(b).Encode(data) // nolint // Only fails due to coding error
 	return b
+}
+
+type StoreMetadata struct {
+	Path          string  `json:"path"`
+	IsPublishable bool    `json:"is_publishable"`
+	CollectionId  *string `json:"collection_id,omitempty"`
+	Title         string  `json:"title"`
+	SizeInBytes   int     `json:"size_in_bytes"`
+	Type          string  `json:"type"`
+	Licence       string  `json:"licence"`
+	LicenceUrl    string  `json:"licence_url"`
+}
+
+type Resumable struct {
+	FileName     string `schema:"resumableFilename"`
+	Type         string `schema:"resumableType"`
+	CurrentChunk int64  `schema:"resumableChunkNumber"`
+	TotalChunks  int    `schema:"resumableTotalChunks"`
+}
+
+type uploadComplete struct {
+	State string `json:"state"`
+	ETag  string `json:"etag"`
+}
+
+type jsonError struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+type jsonErrors struct {
+	Error []jsonError `json:"errors"`
 }
