0ae763b7d20e718f59a372f727b90b695cb97eed
aryan-patel <aryanpatel1771@gmail.com>
2025-10-28 15:54:05 +0000

    make JsonErrors struct globally available for use in sdk

--- api/errors.go
+++ api/errors.go
@@ -8,32 +8,32 @@ import (
 	"github.com/go-playground/validator"
 )
 
-type jsonError struct {
+type JsonError struct {
 	Code        string `json:"code"`
 	Description string `json:"description"`
 }
 
-type jsonErrors struct {
-	Error []jsonError `json:"errors"`
+type JsonErrors struct {
+	Error []JsonError `json:"errors"`
 }
 
-func writeError(w http.ResponseWriter, errs jsonErrors, httpCode int) {
+func writeError(w http.ResponseWriter, errs JsonErrors, httpCode int) {
 	w.WriteHeader(httpCode)
 	if err := json.NewEncoder(w).Encode(&errs); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
-func buildErrors(err error, code string) jsonErrors {
-	return jsonErrors{Error: []jsonError{{Description: err.Error(), Code: code}}}
+func buildErrors(err error, code string) JsonErrors {
+	return JsonErrors{Error: []JsonError{{Description: err.Error(), Code: code}}}
 }
 
-func buildValidationErrors(validationErrs validator.ValidationErrors) jsonErrors {
-	jsonErrs := jsonErrors{Error: []jsonError{}}
+func buildValidationErrors(validationErrs validator.ValidationErrors) JsonErrors {
+	jsonErrs := JsonErrors{Error: []JsonError{}}
 
 	for _, validationErr := range validationErrs {
 		desc := fmt.Sprintf("%s %s", validationErr.Field(), validationErr.Tag())
-		jsonErrs.Error = append(jsonErrs.Error, jsonError{Code: "ValidationError", Description: desc})
+		jsonErrs.Error = append(jsonErrs.Error, JsonError{Code: "ValidationError", Description: desc})
 	}
 	return jsonErrs
 }
